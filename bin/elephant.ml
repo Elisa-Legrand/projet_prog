6b089379125101bb0f63b22577283a2d8244d756
Elisa-Legrand <legrand.elisa.mtp34@gmail.com>
2025-11-12 15:58:49 +0100

    elephant_halfway_debug

--- bin/elephant.ml
+++ bin/elephant.ml
@@ -26,22 +26,26 @@ let straight_line camel_pos elephant_pos =
 (*defines what the elephant will do depending on its state*)
 let rec elephant (current_position : int * int) (current_state : state) =
   match current_state with
-  |Calm -> begin match straight_line !camel_pos current_position with
-    |None -> begin (let new_pos = move_dir current_position random_dir in
+  |Calm -> begin match straight_line camel_pos current_position with
+    |None -> let new_pos = move_dir current_position (random_dir()) in
       render () ; perform End_of_turn ;
-      elephant new_pos Calm) end
-    |Some direction -> elephant current_position Charge(time_charging, direction)
+      elephant new_pos Calm
+    |Some direction -> elephant current_position (Charge(time_charging, direction))
     end
   |Charge(n, direction) when n = 1 -> elephant (move_elephant_charge current_position direction) Calm
-  |Charge(n, direction) when n > 1 -> elephant (move_elephant_charge current_position direction) Charge(n-1, direction)
+  |Charge(n, direction) when n > 1 -> elephant (move_elephant_charge current_position direction) (Charge(n-1, direction))
   |Stunned(n) when n = 1 -> perform End_of_turn ; elephant current_position Calm
-  |Stunned(n) when n > 1 -> perform End_of_turn ; elephant current_position Stunned(n-1)
+  |Stunned(n) when n > 1 -> perform End_of_turn ; elephant current_position (Stunned(n-1))
 (*moves the charging elephant where it needs to be if possible.
 If an entity other than a Cactus is on its way, it will kill it.*)
 and move_elephant_charge elephant_pos direction =
   match elephant_pos ++ dir_to_couple direction with
-  |(x,y) when get(x,y) = Cactus -> elephant current_position Stunned(cooldown_cactus+1)
-  |(x,y) -> kill (x,y)  ; new_position = move_dir elephant_pos direction ;
+  |(x,y)-> match get (x, y) with
+    |(Cactus,)
+  
+  
+  (* get(x,y) = (Cactus, i) -> elephant elephant_pos (Stunned(cooldown_cactus+1))
+  |(x,y) -> kill (x,y)  ; let new_position = move_dir elephant_pos direction in
   render ();
   perform End_of_turn;
   new_position
