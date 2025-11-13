38079b0dbd92e692e2d04de48daaf0ce1001303f
Elisa-Legrand <legrand.elisa.mtp34@gmail.com>
2025-11-13 11:15:31 +0100

    dev spider

--- bin/utils.ml
+++ bin/utils.ml
@@ -100,3 +100,8 @@ let get_random_empty_adjacent_cell (position : int * int) : int * int =
   else
     let idx = Random.int len in
     adjacent_cells.(idx)
+  ;;
+;;
+
+let spawn position entity =
+  set position (entity, prochain_id())
