56cb3e584273e4e82b31d8f86cf1893341a14d23
Elisa-Legrand <legrand.elisa.mtp34@gmail.com>
2025-11-13 16:27:46 +0100

    spawn fix & game over screen

--- bin/player.ml
+++ bin/player.ml
@@ -5,6 +5,8 @@ open Effect
 open Effect.Deep
 open Engine
 
+let number_of_turn_played = ref 0;;
+
 (** [keyboard_direction ()] attend un évènement dans le terminal. Si ECHAP est
     pressée, arrête le jeu. Si une touche directionnelle est pressée, renvoie le
     changement à appliquer sur les coordonnées du chameau pour aller dans la
@@ -22,6 +24,7 @@ let keyboard_direction () : dir =
     la position [current_pos] (attendre une entrée, se déplacer en conséquence,
     recommencer)*)
 let rec camel (current_position : int * int) (id : int) : unit =
+  number_of_turn_played := !number_of_turn_played + 1 ;
   let new_position = move_dir current_position (keyboard_direction ()) in
   camel_pos := new_position;
   render ();
--- bin/spider.ml
+++ bin/spider.ml
@@ -13,13 +13,16 @@ une case adjacente si possible, ou disparaître, ou rien faire). Si [lifetime]
 est un multiple de [spider_egg_cooldown], une araignée apparaît si possible à
 côté. Si [lifetime = spider_egg_lifetime], le sac d'oeufs disparaît. *)
 let rec spider_egg (current_position : int * int) (lifetime : int) id : unit =
-  if lifetime mod 20 = 0 then (
-      let pos_baby = get_random_empty_adjacent_cell current_position in
-  (set pos_baby (Spider, prochain_id ()) ;
-  Queue.add
-    (fun () -> player (fun () -> spider pos_baby (id_courant ())))
-    queue)
-  ) ;
+  try
+    if lifetime mod 20 = 0 then (
+        let pos_baby = get_random_empty_adjacent_cell current_position in
+    (set pos_baby (Spider, prochain_id ()) ;
+    Queue.add
+      (fun () -> player (fun () -> spider pos_baby (id_courant ())))
+      queue)
+    )
+  with
+  | No_adjacent_space -> () ;
   if lifetime >= 60 then kill id ;
   if safe_perform id then spider_egg current_position (lifetime + 1) id
     
@@ -29,11 +32,14 @@ and spider (current_position : int * int) id : unit =
   in
   let spawn_egg_bool = Random.int 100 = 0 in
   if spawn_egg_bool then (
-    let pos_egg = get_random_empty_adjacent_cell current_position in
-    set pos_egg (Spider_Egg, prochain_id ()) ;
-    Queue.add
-      (fun () -> player (fun () -> spider_egg pos_egg 1 (id_courant ())))
-      queue
+    try
+      let pos_egg = get_random_empty_adjacent_cell current_position in
+      set pos_egg (Spider_Egg, prochain_id ()) ;
+      Queue.add
+        (fun () -> player (fun () -> spider_egg pos_egg 1 (id_courant ())))
+        queue
+    with 
+    | No_adjacent_space -> () ;
   );
   if safe_perform id then spider new_position id
 
