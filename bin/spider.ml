498e9893521807296b532d001f755ee3319a8cdf
siruH11 <simon.hannetel@gmail.com>
2025-11-14 10:59:11 +0100

    Fix spider

--- bin/spider.ml
+++ bin/spider.ml
@@ -14,20 +14,28 @@ est un multiple de [spider_egg_cooldown], une araignée apparaît si possible à
 côté. Si [lifetime = spider_egg_lifetime], le sac d'oeufs disparaît. *)
 let rec spider_egg (current_position : int * int) (lifetime : int) id : unit =
   try
-    if lifetime mod spider_egg_cooldown = 0 then
+    if lifetime mod spider_egg_cooldown = 0 then begin
       let pos_baby = get_random_empty_adjacent_cell current_position in
       spawn_spider pos_baby
-  with No_adjacent_space ->
-    ();
-    if lifetime >= spider_egg_lifetime then kill id;
-    if safe_perform id then spider_egg current_position (lifetime + 1) id
+    end
+  else ();
+    if lifetime >= spider_egg_lifetime then begin kill id;
+                                      set current_position (Empty,invalid_id)
+                                      end;
+    if safe_perform id then spider_egg current_position (lifetime+1) id
+  with |No_adjacent_space ->
+    if lifetime >= spider_egg_lifetime then begin kill id;
+                                        set current_position (Empty,invalid_id);
+                                        end;
+    if safe_perform id then spider_egg current_position (lifetime) id
+    
 
 and spider (current_position : int * int) id : unit =
   let (new_position : int * int) = move_dir current_position (random_dir ()) in
   let spawn_egg_bool = Random.int 100 = 0 in
   (if spawn_egg_bool then
      try
-       let pos_egg = get_random_empty_adjacent_cell current_position in
+       let pos_egg = get_random_empty_adjacent_cell new_position in
        spawn_spider_egg pos_egg
      with No_adjacent_space -> ());
   if safe_perform id then spider new_position id
